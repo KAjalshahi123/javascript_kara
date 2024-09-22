15572746add5c5a8926df27ec445eb2c434bf973
Shahikajal <shahikajal274508@gmail.com>
2024-09-22 14:03:06 +0530

    01_datatype

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,13 @@
-let name = "kajal shahi";
+const accountId = 1234;
+var accountName = " Kajal Shahi";
+let accountEmail = " shahikajal274508@gmail.com";
+accountedCity = "delhi";
+
+// cont values does not change
+accountEmail = "shahi@";
+accountName = " Kara";
+
+console.log(accountId);
+console.table([ accountEmail,accountId,accountedCity,accountName])
+
+
