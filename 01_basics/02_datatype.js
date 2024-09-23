111d160ad0f46bd5cd80ec76713e725b68342323
Shahikajal <shahikajal274508@gmail.com>
2024-09-23 09:02:49 +0530

    03_datatypeconversion

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,2 +1,24 @@
-let name = " kajal shahi"
-console.log(name);
+"use strict"; // treat all js node is new
+// alert(3+3)  we are using node,js not browser
+let name = "kajal"
+let age = 14
+let isLooggedIn = false
+
+console.log("name")
+// number ==> 2 to power 53
+// bright
+// string
+// boolean
+// null ==> standalone value
+//undefined ==>
+    // symbol
+// object
+
+console.log( typeof " kajal");
+
+
+
+
+
+
+
